86b2f460e299a4d3419c3bf640501693e30e5d6a
Siddhant Sharma <si.sharma.me@gmail.com>
2026-06-03 14:46:27 +0530

    Fix requirements.txt for deployment

--- hpp_project/settings.py
+++ hpp_project/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'hpp_project.urls'
@@ -135,6 +137,7 @@ ALLOWED_HOSTS = ['*']
 # ✅ Static files
 STATIC_URL  = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ✅ Database — Railway ka PostgreSQL use karo
 import dj_database_url
