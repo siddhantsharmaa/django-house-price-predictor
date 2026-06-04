ab6d845d29095400ba92752a50a5620a3b042455
Siddhant Sharma <si.sharma.me@gmail.com>
2026-06-04 11:08:20 +0530

    Add Cloudinary for media storage

--- hpp_project/settings.py
+++ hpp_project/settings.py
@@ -39,7 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'cloudinary',
+    'cloudinary_storage',
 ]
+
+# ✅ Cloudinary settings
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME' : os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY'    : os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET' : os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+# ✅ Media storage
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
